433fe1c232e90770e8afb6a7915f420378e3163f
SyarifKA <syarif.khalid@gmail.com>
2025-12-02 23:15:37 +0700

    update route

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,33 +1,32 @@
-import { NextResponse } from "next/server";
-import { DataContact } from "@/data/dummy";
+// import { NextResponse } from "next/server";
 
-export function GET() {
-  return NextResponse.json({
-    status: "success",
-    data: DataContact,
-  });
-}
+// export function GET() {
+//   return NextResponse.json({
+//     status: "success",
+//     data: DataContact,
+//   });
+// }
 
-// ADD NEW CONTACT
-export async function POST(request: Request) {
-  const body = await request.json();
-  DataContact.unshift(body);
-  return NextResponse.json({ message: "Contact added", data: body });
-}
+// // ADD NEW CONTACT
+// export async function POST(request: Request) {
+//   const body = await request.json();
+//   DataContact.unshift(body);
+//   return NextResponse.json({ message: "Contact added", data: body });
+// }
 
-// EDIT CONTACT BY INDEX
-export async function PUT(request: Request) {
-  const body = await request.json();
-  const { index, updatedData } = body;
+// // EDIT CONTACT BY INDEX
+// export async function PUT(request: Request) {
+//   const body = await request.json();
+//   const { index, updatedData } = body;
 
-  if (index < 0 || index >= DataContact.length) {
-    return NextResponse.json({ error: "Index not found" }, { status: 400 });
-  }
+//   if (index < 0 || index >= DataContact.length) {
+//     return NextResponse.json({ error: "Index not found" }, { status: 400 });
+//   }
 
-  DataContact[index] = { ...DataContact[index], ...updatedData };
+//   DataContact[index] = { ...DataContact[index], ...updatedData };
 
-  return NextResponse.json({
-    message: "Contact updated",
-    data: DataContact[index],
-  });
-}
+//   return NextResponse.json({
+//     message: "Contact updated",
+//     data: DataContact[index],
+//   });
+// }
